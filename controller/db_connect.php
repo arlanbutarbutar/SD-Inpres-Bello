e76ac2b560296ea4b979b29d875d69ceff15ea7e
arlanbutarbutar <arlan270899@gmail.com>
2022-07-02 20:03:50 +0800

    ubah connect db

--- controller/db_connect.php
+++ controller/db_connect.php
@@ -1,5 +1,6 @@
 <?php 
-  $conn=mysqli_connect("localhost","root","","sd_inpres_bello");
+  $conn=mysqli_connect("localhost","gaslvldx_netmedia","Itha040700_","gaslvldx_sd_inpres_bello");
+  // $conn=mysqli_connect("localhost","root","","sd_inpres_bello");
   if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
   }
